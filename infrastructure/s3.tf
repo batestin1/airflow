08283fe523c8ca26208862117a272ad2894c9853
batestin1 <mayconcipriano@gmail.com>
2022-06-06 18:51:37 -0300

    v1 - 20220606

---
+++ infrastructure/s3.tf
@@ -0,0 +1,25 @@
+resource "aws_s3_bucket" "airflow" {
+  bucket = "lakeabout-tf"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket" "airflowcodes" {
+  bucket = "scripts-tf"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
